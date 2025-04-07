485f39d0a1b43fde3cda9b5c79f9fab0ad0a3772
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2025-04-07 19:28:51 +0200

    feat(chat): implement chat layout with sidebar and cookie-based state management

--- src/app/chat/layout.tsx
+++ src/app/chat/layout.tsx
@@ -1,7 +1,23 @@
-export default function ChatLayout({
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import AppSidebar from "@/components/AppSidebar";
+import { cookies } from "next/headers";
+
+export default async function ChatLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
-	return <div>{children}</div>;
+	const cookieStore = await cookies();
+	const sidebarState = cookieStore.get("sidebar_state");
+	const defaultOpen = sidebarState ? sidebarState.value == "true" : true;
+
+	return (
+		<SidebarProvider defaultOpen={defaultOpen}>
+			<AppSidebar />
+			<main>
+				<SidebarTrigger />
+				{children}
+			</main>
+		</SidebarProvider>
+	);
 }
