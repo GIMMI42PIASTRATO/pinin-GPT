a3c959c72c9392cbe1216672810df84da70cf586
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2025-06-22 19:16:37 +0200

    chore: add .editorconfig and correctly formatted use-mobile.ts

--- src/hooks/use-mobile.ts
+++ src/hooks/use-mobile.ts
@@ -1,19 +1,25 @@
-import * as React from "react"
+import * as React from "react";
 
-const MOBILE_BREAKPOINT = 768
+const MOBILE_BREAKPOINT = 768;
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
+	const [isMobile, setIsMobile] = React.useState<boolean | undefined>(
+		undefined
+	);
 
-  React.useEffect(() => {
-    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
-  }, [])
+	React.useEffect(() => {
+		// Crea una MediaQueryList (mql), che è un oggeto, sul quale è possbile verificare se la MediaQuery specificata in matchMedia corrisponde
+		const mql = window.matchMedia(
+			`(max-width: ${MOBILE_BREAKPOINT - 1}px)`
+		);
+		const onChange = () => {
+			// TODO: Al posto di fare il controllo mauale, è posbbile utilizzare `mql.matches`, che applica la `MediaQuery` specificata come argomento di `window.matchMedia`. Sarebbe più coretto utilizzarlo
+			setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+		};
+		mql.addEventListener("change", onChange);
+		setIsMobile(window.innerWidth < MOBILE_BREAKPOINT);
+		return () => mql.removeEventListener("change", onChange);
+	}, []);
 
-  return !!isMobile
+	return !!isMobile;
 }
