08ac3c9452df011b706fad431160bdfb2ceda58c
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2025-06-26 16:30:38 +0200

    feat: centered the caht

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -21,7 +21,7 @@ export default function ChatHome() {
 					</ChatContainer>
 					{/* </MaxWidthWrapper> */}
 				</div>
-				<div className="fixed bottom-0 left-0 w-full px-2 z-50 bg-white/80 backdrop-blur supports-[backdrop-filter]:bg-white/60">
+				<div className="fixed bottom-0 left-0 w-full ml-[150px] px-2 z-50 bg-white/80 backdrop-blur supports-[backdrop-filter]:bg-white/60">
 					<div className="relative mx-auto max-w-3xl">
 						<InputPrompt />
 					</div>
